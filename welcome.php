ccb8711ede2422931a41d9cb3c46acd25c1affa1
Marcial Mendoza <MendozaM0717@gmail.com>
2020-10-24 02:51:27 -0700

    login system

---
+++ welcome.php
@@ -0,0 +1,12 @@
+<!-- welcome.php -->
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>Welcome</title>
+</head>
+<body>
+    <img src="https://www.callcentrehelper.com/images/stories/2018/11/welcome-red-sign-760.png" alt="welcome" /img>
+</body>
+</html>
