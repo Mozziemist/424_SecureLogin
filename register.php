078d4b34b8b0add682dd07f0dc74db3ff5c12fd0
Marcial Mendoza <MendozaM0717@gmail.com>
2020-11-05 15:59:55 -0800

    debug

--- register.php
+++ register.php
@@ -79,6 +79,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             // Attempt to execute the prepared statement
             if(mysqli_stmt_execute($stmt)){
                 // Redirect to login page
+                echo "Something went right"
                 header("location: index.php");
             } else{
                 echo "Something went wrong. Please try again later.";
