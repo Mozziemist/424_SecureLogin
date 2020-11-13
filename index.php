1717725b33775120abe46bf21ff4979a4149f09b
Kevin <kehvout@gmail.com>
2020-11-13 14:57:14 -0800

    updatin

--- index.php
+++ index.php
@@ -55,7 +55,11 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                     // Bind result variables
                     mysqli_stmt_bind_result($stmt, $id, $username, $hashed_password);
                     if(mysqli_stmt_fetch($stmt)){
-                        if(password_verify($password, $hashed_password)){
+			if(password_verify($password, $hashed_password)){
+			    // Check if account is activated
+			    $act_check = "SELECT * FROM users WHERE username = '$username' AND activated = 1 LIMIT 1";
+			    $act_result = mysqli_query($link, $act_check);
+			    if(mysqli_num_rows($act_result) > 0){
                             // Password is correct, so start a new session
                             session_start();
                             
@@ -92,7 +96,10 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                             $_SESSION["login_count"] = $login_count[0];
 
                             // Redirect user to welcome page
-                            header("location: welcome.php");
+			    header("location: welcome.php");
+			    } else {
+			        $activation_err = "Please activate your account before logging in.";
+			    }
                         } else{
                             // Display an error message if password is not valid
                             $password_err = "The password you entered was not valid.";
@@ -121,7 +128,10 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 <head>
     <meta charset="UTF-8">
     <title>Login</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <style type="text/css">
         body{ font: 14px sans-serif; }
         .wrapper{ width: 350px; padding: 20px; }
@@ -129,18 +139,20 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 </head>
 <body>
     <div class="wrapper">
+    <div class="alert alert-warning" role="alert"><?php echo $activation_err; ?>
+	</div>
         <h2>Login</h2>
         <p>Please enter your credentials to login.</p>
         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
             <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
                 <label>Username</label>
                 <input type="text" name="username" class="form-control" value="<?php echo $username; ?>">
-                <span class="help-block"><?php echo $username_err; ?></span>
+                <span class="form-text" style="color:red"><?php echo $username_err; ?></span>
             </div>    
             <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
                 <label>Password</label>
                 <input type="password" name="password" class="form-control">
-                <span class="help-block"><?php echo $password_err; ?></span>
+                <span class="form-text" style="color:red"><?php echo $password_err; ?></span>
             </div>
             <div class="form-group">
                 <input type="submit" class="btn btn-primary" value="Login">
